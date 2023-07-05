ed925a63acdbd615a99262a8a3a0de39db0cf11a
전우진 <woojin97318@gmail.com>
2023-07-05 18:29:31 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 224 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int king = sc.nextInt();
+        int queen = sc.nextInt();
+        int rook = sc.nextInt();
+        int bishop = sc.nextInt();
+        int knight = sc.nextInt();
+        int pawn = sc.nextInt();
+        
+        System.out.print(1 - king + " ");
+        System.out.print(1 - queen + " ");
+        System.out.print(2 - rook + " ");
+        System.out.print(2 - bishop + " ");
+        System.out.print(2 - knight + " ");
+        System.out.print(8 - pawn);
+    }
+}
