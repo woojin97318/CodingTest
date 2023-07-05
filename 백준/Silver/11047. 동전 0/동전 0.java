7f26a2d66cea6ada249eb59304a16dca3a8770a5
전우진 <woojin97318@gmail.com>
2023-07-05 18:30:50 +0900

    [Silver IV] Title: 동전 0, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int cnt = 0;
+        for(int i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] <= k) {
+                cnt += k / arr[i];
+                k %= arr[i];
+            }
+        }
+        
+        System.out.print(cnt);
+    }
+}
