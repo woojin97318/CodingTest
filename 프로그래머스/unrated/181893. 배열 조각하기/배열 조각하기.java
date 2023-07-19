be5b8ddcf1cf91027c00d2ff275bf49be111d41c
전우진 <woojin97318@gmail.com>
2023-07-19 19:48:56 +0900

    Update 배열 조각하기.java
    리팩토링

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -1,6 +1,5 @@
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
@@ -11,32 +10,14 @@ class Solution {
         
         for (int i = 0; i < query.length; i++) {
             int query_num = query[i];
+            
             if (i % 2 == 0) {
-                // 짝수 인덱스일 경우, 뒷부분을 잘라냄
                 list.subList(query_num + 1, list.size()).clear();
             } else {
-                // 홀수 인덱스일 경우, 앞부분을 잘라냄
                 list.subList(0, query_num).clear();
             }
         }
 
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
-        }
-
-        return result;
-        
-//         for (int i = 0; i < query.length; i++) {
-//             int query_num = query[i];
-            
-//             if (i % 2 == 0) {
-//                 list = list.subList(0, query_num);
-//             } else {
-//                 list = list.subList(query_num + 1, list.size());
-//             }
-//         }
-        
-        // return list.stream().mapToInt(Integer::intValue).toArray();
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
