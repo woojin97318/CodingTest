c19039be4181b9794ede269bd56ba23248ca3aef
전우진 <woojin97318@gmail.com>
2023-07-21 10:04:03 +0900

    [unrated] Title: 할 일 목록, Time: 1.32 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i])
+                answer.add(todo_list[i]);
+        }
+        
+        return answer.stream().toArray(String[]::new);
+    }
+}
