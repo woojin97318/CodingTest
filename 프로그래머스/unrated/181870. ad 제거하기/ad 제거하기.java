f3af479e6796e00acd320749ec8f056ca020cc88
전우진 <woojin97318@gmail.com>
2023-07-23 20:48:45 +0900

    [unrated] Title: ad 제거하기, Time: 2.65 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        return Arrays.stream(strArr)
+            .filter(str -> !str.contains("ad"))
+            .toArray(String[]::new);
+    }
+}
