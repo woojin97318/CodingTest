bd94eb601223e725ffc3cd132fcc75eb86c0645d
전우진 <woojin97318@gmail.com>
2023-07-23 20:53:03 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 2.43 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        return Arrays.stream(my_string.split(" "))
+            .filter(str -> !str.equals(""))
+            .toArray(String[]::new);
+    }
+}
