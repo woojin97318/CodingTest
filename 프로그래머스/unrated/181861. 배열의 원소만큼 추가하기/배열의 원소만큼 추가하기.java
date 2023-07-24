b821a94669138d29c91aa2eb7675f63eb5c9a2c2
전우진 <woojin97318@gmail.com>
2023-07-24 22:47:22 +0900

    [unrated] Title: 배열의 원소만큼 추가하기, Time: 1.97 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int num : arr) {
+            for (int i = 0; i < num; i++) {
+                list.add(num);
+            }
+        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
