c8277fbb3051c415472ae0a8fe5bea0e3b1d04a9
전우진 <woojin97318@gmail.com>
2023-08-02 20:52:55 +0900

    [level 0] Title: 옹알이 (1), Time: 0.43 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        
+        for (String str : babbling) {
+            str = str.replace("aya", ".");
+            str = str.replace("ye", ".");
+            str = str.replace("woo", ".");
+            str = str.replace("ma", ".");
+            
+            if (str.matches("\\.+"))
+                answer++;
+        }
+        
+        return answer;
+    }
+}
