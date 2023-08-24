d3cb63bca570a799b4a7f8357eb2fe34f76bd99f
전우진 <woojin97318@gmail.com>
2023-08-24 22:34:07 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.30 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] strArr = my_string.split("");
+        
+        String temp = strArr[num1];
+        strArr[num1] = strArr[num2];
+        strArr[num2] = temp;
+        
+        return String.join("", strArr);
+    }
+}
