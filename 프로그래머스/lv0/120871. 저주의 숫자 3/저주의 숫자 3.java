ed9aebf17811aa3dc52552d29f2b9867c697090c
전우진 <woojin97318@gmail.com>
2023-09-16 11:29:52 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        
+        for (int i = 1; i <= n; i++, answer++) {
+            while (answer % 3 == 0 || answer % 10 == 3 || answer / 10 == 3 || answer / 10 == 13) {
+                answer++;
+            }
+        }
+        
+        return --answer;
+    }
+}
