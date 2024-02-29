082683c724977c9a3b7bcc5f1382d25d683e45f4
전우진 <woojin97318@gmail.com>
2024-02-29 16:01:27 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 5.24 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(String my_string) {
+        return my_string.chars()
+            .filter(Character::isDigit)
+            .map(Character::getNumericValue)
+            .sorted()
+            .toArray();
+    }
+}
