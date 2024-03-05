6a9bd798891c600c8445007cb0bcdc40765a4d33
전우진 <woojin97318@gmail.com>
2024-03-05 13:12:20 +0900

    [level 0] Title: 소인수분해, Time: 2.39 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -10,16 +10,14 @@ class Solution {
             n /= 2;
         }
         
-        for (int i = 3; i <= n * n; i += 2) {
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
             while (n % i == 0) {
                 set.add(i);
                 n /= i;
             }
         }
         
-        if (n > 2) {
-            set.add(n);
-        }
+        if (n > 2) set.add(n);
         
         return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
