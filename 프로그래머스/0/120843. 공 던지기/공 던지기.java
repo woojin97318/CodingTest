f4fba6675a8bed17372e1baf60714f13888e0964
전우진 <woojin97318@gmail.com>
2024-03-06 13:20:17 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,14 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int len = numbers.length;
+        
+        for (int i = 0; i < k * 2; i += 2) {
+            answer = numbers[i % len];
+        }
+        
+        return answer;
+    }
+}
