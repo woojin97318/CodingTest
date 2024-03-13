64d601574d6b96bfa2f33de4dcb91633cde82959
전우진 <woojin97318@gmail.com>
2024-03-13 13:39:08 +0900

    [level 1] Title: 하샤드 수, Time: 0.11 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,9 +1,20 @@
 class Solution {
     public boolean solution(int x) {
+        /** for문을 이용한 방법 */
         int sum = 0;
         for (String num : String.valueOf(x).split("")) {
             sum += Integer.valueOf(num);
         }
+        
+        /** stream을 이용한 방법 1 */
+        // int sum = String.valueOf(x).chars()
+        //     .map(ch -> ch - '0')
+        //     .sum();
+        
+        /** stream을 이용한 방법 2 */
+        // int sum = String.valueOf(x).chars()
+        //     .map(Character::getNumericValue)
+        //     .sum();
         
         return x % sum == 0 ? true : false;
     }
