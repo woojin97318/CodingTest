567237712ac14e822a0771213270a9916ac155b4
전우진 <woojin97318@gmail.com>
2024-03-25 14:40:33 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.32 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -4,13 +4,31 @@ class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         
-        List<Integer> hallOfFame = new ArrayList<>();
+        PriorityQueue<Integer> hallOfFame = new PriorityQueue<>();
         for (int i = 0; i < score.length; i++) {
             hallOfFame.add(score[i]);
-            Collections.sort(hallOfFame, Collections.reverseOrder());
-            answer[i] = hallOfFame.get(hallOfFame.size() < k ? hallOfFame.size() - 1 : k - 1);
+            
+            if (hallOfFame.size() > k)
+                hallOfFame.poll();
+            
+            answer[i] = hallOfFame.peek();
         }
         
         return answer;
     }
 }
+
+// class Solution {
+//     public int[] solution(int k, int[] score) {
+//         int[] answer = new int[score.length];
+        
+//         List<Integer> hallOfFame = new ArrayList<>();
+//         for (int i = 0; i < score.length; i++) {
+//             hallOfFame.add(score[i]);
+//             Collections.sort(hallOfFame, Collections.reverseOrder());
+//             answer[i] = hallOfFame.get(hallOfFame.size() < k ? hallOfFame.size() - 1 : k - 1);
+//         }
+        
+//         return answer;
+//     }
+// }
