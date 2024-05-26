1b7095235f82e3d513b513b64640de5048862849
전우진 <woojin97318@gmail.com>
2024-05-26 20:16:09 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.54 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -7,35 +7,44 @@ class Solution {
         // report 중복 제거
         Set<String> reportSet = new HashSet<>(Arrays.asList(report));
 
-        // k번 이상 신고 당한 사람
-        List<String> kHuman = new ArrayList<>();
-        Map<String, Integer> kmap = new HashMap<>();
-        for (String str : reportSet) {
-            kmap.put(str.split(" ")[1], kmap.getOrDefault(str.split(" ")[1], 0) + 1);
-        }
-
-        for (Map.Entry<String, Integer> entry : kmap.entrySet()) {
-            if (entry.getValue() >= k) {
-                kHuman.add(entry.getKey());
-            }
-        }
-
-        // key: 유저 ID, value: 정지된 ID
-        Map<String, Integer> map = new LinkedHashMap<>();
-        for (String id : id_list) {
-            map.put(id, 0);
-        }
-
-        for (String str : reportSet) {
-            if (kHuman.contains(str.split(" ")[1])) {
-                String key = str.split(" ")[0];
-                map.put(key, map.getOrDefault(key, 0) + 1);
-            }
-        }
+        // k번 이상 신고 당한 사람을 저장하는 리스트
+        List<String> suspendedUserList = new ArrayList<>();
         
+        // 신고된 횟수를 저장하는 맵
+        Map<String, Integer> reportCountMap = new HashMap<>();
+        for (String str : reportSet) {
+            String reportedUser = str.split(" ")[1];
+            reportCountMap.put(reportedUser, reportCountMap.getOrDefault(reportedUser, 0) + 1);
+        }
+
+        // k번 이상 신고 당한 사용자를 suspendedUserList에 추가
+        for (Map.Entry<String, Integer> entry : reportCountMap.entrySet()) {
+            if (entry.getValue() >= k) {
+                suspendedUserList.add(entry.getKey());
+            }
+        }
+
+        // 각 사용자가 신고한 정지된 사용자 수를 저장하는 맵
+        Map<String, Integer> userReportMap = new LinkedHashMap<>();
+        for (String id : id_list) {
+            userReportMap.put(id, 0);
+        }
+
+        // 정지된 사용자들을 신고한 사용자에 대한 처리
+        for (String str : reportSet) {
+            String[] splitStr = str.split(" ");
+            String reportingUser = splitStr[0];
+            String reportedUser = splitStr[1];
+
+            if (suspendedUserList.contains(reportedUser)) {
+                userReportMap.put(reportingUser, userReportMap.getOrDefault(reportingUser, 0) + 1);
+            }
+        }
+
+        // 결과 배열에 각 사용자가 신고한 정지된 사용자 수를 저장
         int idx = 0;
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            answer[idx++] = entry.getValue();
+        for (int count : userReportMap.values()) {
+            answer[idx++] = count;
         }
 
         return answer;
