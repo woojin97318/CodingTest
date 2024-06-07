61ece88b7cbf26fa03af7ec969a327d080f4ae81
전우진 <woojin97318@gmail.com>
2024-06-07 23:57:09 +0900

    [level 2] Title: 올바른 괄호, Time: 14.69 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                deque.push(c);
+            } else if (c == ')') {
+                if (deque.isEmpty()) {
+                    return false;
+                }
+                deque.pop();
+            }
+        }
+
+        return deque.isEmpty();
+    }
+}
