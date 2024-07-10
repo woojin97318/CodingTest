b0744e8053418babc2b541dfa0521e03f2c0ebda
전우진 <woojin97318@gmail.com>
2024-07-10 22:46:00 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.35 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,29 +1,14 @@
 class Solution {
     public String solution(String s) {
-        StringBuilder answer = new StringBuilder();
-
-        String[] sArr = s.split(" ", -1);
+        String answer = "";
+        String[] sArr = s.toLowerCase().split("");
+        boolean flag = true;
         
-        for (int i = 0; i < sArr.length; i++) {
-            String str = sArr[i];
-            
-            if (!str.equals("")) {
-                String firstStr = str.substring(0, 1);
-            
-                if (firstStr.matches("[a-zA-Z]")) {
-                    answer
-                        .append(firstStr.toUpperCase())
-                        .append(str.substring(1).toLowerCase());
-                } else {
-                    answer.append(str.toLowerCase());
-                }
-            }
-            
-            if (i < sArr.length - 1) {
-                answer.append(" ");
-            }
+        for (String str : sArr) {
+            answer += flag ? str.toUpperCase() : str;
+            flag = str.equals(" ") ? true : false;
         }
         
-        return answer.toString();
+        return answer;
     }
 }
